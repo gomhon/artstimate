6d77f7b74cd0da98b4cebe969e173e8cf610ae95
Alex <ababich@asu.edu>
2013-12-26 12:27:22 -0700

    Project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artstimate::Application.config.secret_key_base = '8828776eca44c076a4d656baeadb817ef5773c5be308ca956b943fcef0d659b9e1eb310cf790be2cf19928bffa8eee80db2c6a315436f82696e638bd38223a63'
